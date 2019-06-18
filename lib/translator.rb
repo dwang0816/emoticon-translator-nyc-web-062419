09c86141f0d0389737890b07135b531465e06dbd
dennis wang <dwang0816@gmail.com>
2019-06-18 18:54:50 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,14 @@
 # require modules here
-
+require "yaml"
 def load_library
   # code goes here
+  ontain = {"get_meaning"=>{},"get_emoticon"=>{}} 
+  emoticons = YAML.load_file lib
+  emoticons.each do |define,arr|
+    contain["get_meaning"][arr[1]] = define
+    contain["get_emoticon"][arr[0]] = arr[1]
+  end
+  contain
 end
 
 def get_japanese_emoticon
